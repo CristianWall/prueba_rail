b0b84351e3df2a02e4cdd70be94980ae93d43983
Cristian Walter <crisvi072@gmail.com>
2025-10-21 09:48:53 -0500

    Fix Railway deployment configuration

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ def info():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
